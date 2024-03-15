4ba6f3e4dd4fa7b450d63628c1bbbd34e2281766
Everton Gomes <evertongn@hotmail.com>
2024-03-14 21:19:38 -0300

    Login

---
+++ src/Intermediarios/autenticacao.js
@@ -0,0 +1,33 @@
+const knex = require("../conexao")
+const jwt = require("jsonwebtoken")
+const senhaJwt = process.env.SENHA_JWT;
+
+
+
+const autenticacao = async (req, res, next) => {
+    const bearerToken = req.headers.authorization;
+
+    if (!bearerToken) {
+        return res.status(401).json({ mensagem: 'Autenticação Não encontrada' });
+    }
+
+    const token = bearerToken.replace('Bearer ', '').trim();
+
+    try {
+        const { id } = jwt.verify(token, senhaJwt)
+
+        const usuarioEncontrado = await knex('usuarios').where({ id }).first()
+
+        if (!usuarioEncontrado) {
+            return res.status(404).json({ mensagem: 'O Usuario não foi encontrado' });
+        }
+
+        const { senha, ...usuario } = usuarioEncontrado;
+        req.usuario = usuario
+        next()
+    } catch (error) {
+        return res.status(500).json({ mensagem: error.detail });
+    }
+}
+
+module.exports = autenticacao
--- src/rotas.js
+++ src/rotas.js
@@ -1,23 +1,38 @@
 const express = require("express")
 const rotas = express.Router()
 
+//autenticacao
+const autenticacao = require("./Intermediarios/autenticacao")
+
 //Roles
 const { cadatroRotes, listarRotes, deleteRotes } = require("./Controladores/rotes")
 
 //Usuarios 
 const { listarUsuarios, listarUsuario, cadastrarUsuario, editararUsuario, excluirUsuario } = require("./Controladores/usuarios")
 
+//login
+const login = require("./Controladores/login")
+
+//---------------------------Rotas--------------------------------------
+
 //Rotas-Roles
 rotas.get('/roles', listarRotes)
 rotas.post('/roles', cadatroRotes)
 rotas.delete('/roles/:id', deleteRotes);
 
+//login
+rotas.post('/login', login)
+
+//Autenticação JWT
+rotas.use(autenticacao);
+
+//Rotas Protegidas por token -----------------------------------
+
 //Rotas-Usuarios
+rotas.post('/usuario', cadastrarUsuario)
 rotas.get('/usuario', listarUsuarios)
 rotas.get('/usuario/:id', listarUsuario)
-rotas.post('/usuario', cadastrarUsuario)
 rotas.put('/usuario/:id', editararUsuario)
 rotas.delete('/usuario/:id', excluirUsuario)
-
 
 module.exports = rotas
